4f71d19acd44b185685923e268eadc906fee56c9
Cirill <Sparta-sparta158@mail.ru>
2021-05-23 22:43:51 +0300

    3-modules

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,10 @@
 function factorial(n) {
   // ваш код...
+  let a = 1;
+  while(n) a*= n --;
+  return a
 }
+factorial(0);
+factorial(1);
+factorial(3);
+factorial(5);
--- 2-module/2-task/index.js
+++ 2-module/2-task/index.js
@@ -1,3 +1,13 @@
-function isEmpty(obj) {
-  // ваш код...
-}
+function isEmpty(obj) 
+{for (const i in obj) {
+  if (obj.hasOwnProperty(i)) {
+    return false;
+  }
+} return true; }
+let schedule = {};
+
+alert( isEmpty(schedule) );
+
+schedule["8:30"] = "подъём";
+
+alert( isEmpty(schedule) )
